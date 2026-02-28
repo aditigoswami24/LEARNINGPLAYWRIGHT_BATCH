f56daeddc78bc01040a5c8bfba93dcf339f523a9
auto-commit <devnull@example.com>
2026-02-27 19:08:26 -0500

    Update workspace changes

---
+++ chapter_01_basics/Chapter_02/05_Core_commentJS.js
@@ -0,0 +1,15 @@
+// this is comment in JavaScript this code not execute
+console.log("Hello World");  
+/**
+ * This is multi line comment in JavaScript
+ * Author: Aditi Goswami
+ * Date: 2024-06-01
+ * Description: This code demonstrates the use of comments in JavaScript.
+ **/
+/*
+ * This is multi line comment in JavaScript
+ * Author: Aditi Goswami
+ * Date: 2024-06-01
+ * Description: This code demonstrates the use of comments in JavaScript.
+ */
+ 
---
+++ chapter_01_basics/Chapter_02/chaper_02_java_concepts/06_Core_identifier_JS.js
@@ -0,0 +1,44 @@
+// Identifiers, Literals, and operators
+
+ var a = 10; // a is an identifier, 10 is a literal
+ a = 20
+console.log(a); 
+// variable name = identifier - a name which is given to a container data in it. 
+// 10 is a variable value = literal 
+// = is an operator - a symbol which performs some operation on operands.
+
+
+var a1 = 10 ;
+console.log(a1); 
+
+// rules of identifiers in JavaScript:
+// 1. Identifiers can contain letters, digits, underscores, and dollar signs.
+// 2. Identifiers cannot start with a digit.
+// 3. Identifiers are case-sensitive.
+// 4. Identifiers cannot be reserved keywords in JavaScript (e.g., var, let, const, if, else, etc.).
+
+var name = 20;
+console.log(name);
+
+var _name = 30;
+console.log(_name);
+
+var $name = 40;
+console.log($name);
+
+var name1 = 50;
+console.log(name1);
+
+var NAME = "Aditi";
+console.log(NAME);
+var name = "Goswami";
+console.log(name);
+
+var π = 3.14;
+console.log(π);
+
+var नमस्ते = "Hello";
+console.log(नमस्ते);
+ 
+//var break = " lets go
+//console.log(break); //will cause a syntax error because 'break' is a reserved keyword 
